7883ef37b773d84ffc19a56552c5e51f0b834b99
lan-sky <15281040@bjtu.edu.cn>
2021-01-11 00:40:55 +0800

    'transformer_v2_cross'

--- configs/pointpillars/pointpillars_with_img.py
+++ configs/pointpillars/pointpillars_with_img.py
@@ -93,9 +93,9 @@ optimizer = dict(lr=lr)
 # specifically tune this parameter.
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # Use evaluation interval=2 reduce the number of evaluation timese
-evaluation = dict(interval=5)
+evaluation = dict(interval=160)
 # PointPillars usually need longer schedule than second, we simply double
 # the training schedule. Do remind that since we use RepeatDataset and
 # repeat factor is 2, so we actually train 160 epochs.
-total_epochs = 80
+total_epochs = 160
 find_unused_parameters = True
